595eea58aec0f50a5d061193eefca606d1b4be7a
H_Larsson <henrik.larsson.4@volvocars.com>
2019-05-14 12:25:41 +0200

    First commit

---
+++ PendelParkering/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PendelParkering
+//
+//  Created by Larsson, Henrik (94777) on 2019-05-14.
+//  Copyright © 2019 Larsson, Henrik (94777). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var fullFormArray = Array<String>()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fullFormArray.append("object 1")
+        fullFormArray.append("object 2")
+        fullFormArray.append("object 3")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
