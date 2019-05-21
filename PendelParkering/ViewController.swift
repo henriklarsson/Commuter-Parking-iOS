9d960e321ea208392cab9f4c6c18eb8ba8493eae
H_Larsson <henrik.larsson.4@volvocars.com>
2019-05-21 06:29:44 +0200

    Added parking

--- PendelParkering/ViewController.swift
+++ PendelParkering/ViewController.swift
@@ -8,15 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    var fullFormArray = Array<String>()
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+    var parkings = [ParkingLot]()
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        fullFormArray.append("object 1")
-        fullFormArray.append("object 2")
-        fullFormArray.append("object 3")
+//        super.viewDidLoad()
+//        public var lat: Double
+//        public var _id: Int
+//        /** Only available if ParkingType Name is SMARTCARPARK */
+//        public var parkingCameras: [ParkingCamera]?
+//        public var lon: Double
+//        public var isRestrictedByBarrier: Bool
+//        /** Number of free spaces. Only available if ParkingType Name is SMARTCARPARK */
+//        public var freeSpaces: Int?
+        let parkingType = ParkingType(name: ParkingType.Name.smartcarpark, number: 1)
+        let parking1 = ParkingLot.init(name: "ParkingSpaceName", totalCapacity: 2, parkingType: parkingType, lat: Double(2), _id: 3, parkingCameras: nil, lon: Double(3), isRestrictedByBarrier: false, freeSpaces: 2)
+        parkings.append(parking1)
         // Do any additional setup after loading the view.
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return parkings.count
+    }
+    
+    
+   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
+        
+        cell.textLabel?.text = parkings[indexPath.row].name
+        
+        return cell
     }
 
 
